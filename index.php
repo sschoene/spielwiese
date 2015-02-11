ec1b782a6cb18006c363d92af04c2dca28699b2f
Steffen Schoene <Steffen.Schoene@t-systems.com>
2015-02-11 14:33:28 +0100

    add landing page, add dialog demo

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<title>Info Page</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+		
+		<link rel="stylesheet" href="style.css">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<header class="navbar navbar-static-top bs-docs-nav" id="top">
+			<div class="container-fluid">
+				<div class="navbar-header">
+			      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			      <a href="#" class="navbar-brand">Home</a>
+			    </div>
+				<nav class="collapse navbar-collapse bs-navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#" class="navbar-brand">Nav 1</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#" class="navbar-brand">Meta Nav 1</a>
+						</li>
+					</ul>
+				</nav>
+			</dib>
+		</header>
+
+		<main class="bs-docs-masthead" id="content" role="main">
+			<div class="container-fluid">
+				<h1>Spielwiese</h1>
+			</div>
+		</main>
+		<div class="container-fluid bs-docs-featurette">
+			<dl class="dl-horizontal">
+				  <dt>Dialog</dt>
+				  <dd><a href="demos/dialog.html">Demo</a></dd>
+				</dl>
+		</div>
+
+		<footer class="bs-docs-footer" role="contentinfo">
+			<div class="container-fluid">
+				FOOTER
+			</div>
+		</footer>
+
+
+		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	</body>
+</html>
